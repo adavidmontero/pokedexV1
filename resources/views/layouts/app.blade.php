4ebaf3c7ed0f03c71bfa34ab61431e8b4304ae0d
adavidmontero <67876368+adavidmontero@users.noreply.github.com>
2021-09-03 11:40:38 -0500

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"> 
+        <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 
         <title>Pokedex</title>
 
@@ -16,7 +16,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('/css/app.css') }}" rel="stylesheet" type="text/css" />
 
         <style>
             body {
