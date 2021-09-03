74fbee44b183b529041fd38d8ad43a65cda5d963
adavidmontero <67876368+adavidmontero@users.noreply.github.com>
2021-09-03 11:25:59 -0500

    .

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet" type="text/css" />
 
         <style>
             body {
